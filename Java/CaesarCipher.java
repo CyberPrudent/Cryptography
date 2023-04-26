d87003014f80f2c556d1f78d78f2ff6d89daf02d
CyberPrudent <125636433+CyberPrudent@users.noreply.github.com>
2023-04-26 08:25:39 -0600

    Update CaesarCipher.java

--- Java/CaesarCipher.java
+++ Java/CaesarCipher.java
@@ -13,36 +13,52 @@
 
 public class CaesarCipher {
     
+    // Encrypts the message using the Caesar Cipher algorithm
     public static String encrypt(String message, int key) {
         String encrypted = "";
+        // Iterate through each character in the message
         for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
+            // If the character is a letter, shift it by the key value
             if (Character.isLetter(ch)) {
+                // Convert the character to uppercase and shift it by the key value
                 ch = (char) (((ch - 'A' + key) % 26) + 'A');
             }
+            // Add the shifted character to the encrypted message
             encrypted += ch;
         }
+        // Return the encrypted message
         return encrypted;
     }
     
+    // Decrypts the message using the Caesar Cipher algorithm
     public static String decrypt(String message, int key) {
         String decrypted = "";
+        // Iterate through each character in the message
         for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
+            // If the character is a letter, shift it back by the key value
             if (Character.isLetter(ch)) {
+                // Convert the character to uppercase and shift it back by the key value
                 ch = (char) (((ch - 'A' - key + 26) % 26) + 'A');
             }
+            // Add the shifted character to the decrypted message
             decrypted += ch;
         }
+        // Return the decrypted message
         return decrypted;
     }
     
     public static void main(String[] args) {
+        // Define the message and key values
         String message = "HELLO WORLD";
         int key = 3;
+        // Encrypt the message using the key value
         String encryptedMessage = encrypt(message, key);
+        // Decrypt the encrypted message using the same key value
         String decryptedMessage = decrypt(encryptedMessage, key);
         
+        // Print the original message, encrypted message, and decrypted message
         System.out.println("Original message: " + message);
         System.out.println("Encrypted message: " + encryptedMessage);
         System.out.println("Decrypted message: " + decryptedMessage);
